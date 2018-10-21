0eed85947f66783652354f4c398e51afbb2fd7ee
njpowers7915 <njpowers7915@gmail.com>
2018-10-21 09:38:17 -0400

    Created Playlist and Track Controllers

---
+++ nicks-playlist-app-api/app/controllers/api/playlists_controller.rb
@@ -0,0 +1,49 @@
+class Api::PlaylistsController < ApplicationController
+  #before_action :require_login, except: [:index, :show]
+  before_action :set_playlist, only: [:show, :update, :destroy]
+
+  def index
+    @playlists = Playlist.all
+    render json: { playlists: playlists }
+  end
+
+  def create
+    playlist = Playlist.new(playlist_params)
+    if playlist.save
+      render json: playlist
+    else
+      render json: { message: 'Could not create playlist!' }
+    end
+  end
+
+  def show
+    render json: @playlist
+  end
+
+  def update
+    if @playlist.update(playlist_params)
+      render json: @playlist
+    else
+      render json: { message: @playlist.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @playlist.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to remove this playlist" }, status: 400
+    end
+  end
+
+  private
+
+    def set_playlist
+      @playlist = Playlist.find_by(id: params[:id])
+    end
+
+    def playlist_params
+      params.require(:playlist).permit(:name, :description)
+    end
+
+end
