509bd34b7aac2ce9ef0657180a25afe58c1d6ccb
njpowers7915 <njpowers7915@gmail.com>
2018-10-21 09:14:56 -0400

    Set up relationships between Playlists and Tracks

--- nicks-playlist-app-api/app/models/playlist.rb
+++ nicks-playlist-app-api/app/models/playlist.rb
@@ -1,2 +1,5 @@
 class Playlist < ApplicationRecord
+  #belongs_to :user
+  has_many :playlists_tracks
+  has_many :tracks, through: :playlists_tracks
 end
--- nicks-playlist-app-api/db/migrate/20181021130915_tracks_playlists.rb
+++ nicks-playlist-app-api/db/migrate/20181021130915_tracks_playlists.rb
@@ -1,4 +1,10 @@
 class TracksPlaylists < ActiveRecord::Migration[5.2]
   def change
+    create_table :tracks_playlists do |t|
+      t.integer :track_id
+      t.integer :playlist_id
+
+      t.timestamps
+    end
   end
 end
