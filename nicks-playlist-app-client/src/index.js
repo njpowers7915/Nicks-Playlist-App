1d1759a9f6fda663060098c4e824ed6fc80417f7
njpowers7915 <njpowers7915@gmail.com>
2018-10-21 10:16:41 -0400

    Set up index.js

--- nicks-playlist-app-client/src/index.js
+++ nicks-playlist-app-client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import store from './store.js'
+import App from './containers/App';
+import registerServiceWorker from './serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+
+    <div>
+      <App />
+    </div>,
+    
+  document.getElementById('root')
+);
+
+registerServiceWorker();
