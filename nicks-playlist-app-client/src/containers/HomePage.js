57bf8ac1300a80d2f52da5d57b9371b6caa2fab4
njpowers7915 <njpowers7915@gmail.com>
2018-10-21 12:53:33 -0400

    Adding routes to HomePage Container

--- nicks-playlist-app-client/src/containers/HomePage.js
+++ nicks-playlist-app-client/src/containers/HomePage.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import PlaylistList from '../components/PlaylistList';
+//import { connect } from 'react-redux';
+import { Route, Switch, Link } from 'react-router-dom';
+
+import NewPlaylistForm from '../components/NewPlaylistForm'
+import PlaylistList from '../components/PlaylistList'
+import PlaylistComponent from '../components/PlaylistComponent'
 
 
 class HomePage extends Component {
   render() {
+    const { match } = this.props;
+    
     return (
       <div className="home-page">
-        Link to Playlist Form
-        <PlaylistList />
+        <Route exact path={match.url} render={() =>
+            <Link to="/playlists/new">Create a Playlist!</Link>} />
+        <Switch>
+          <Route exact path={match.url} component={PlaylistList} />
+          <Route exact path={match.url + '/new'} component={NewPlaylistForm} />
+          <Route exact path={match.url + '/:playlistId'} component={PlaylistComponent} />
+        </Switch>
       </div>
     );
   }
