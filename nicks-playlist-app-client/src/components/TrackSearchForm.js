562a7f87628381be159836a7754bd2f6d92e944c
njpowers7915 <njpowers7915@gmail.com>
2018-10-22 10:46:00 -0400

    Making Track Search Form

--- nicks-playlist-app-client/src/components/TrackSearchForm.js
+++ nicks-playlist-app-client/src/components/TrackSearchForm.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import {fetchTracks} from '../actions/trackActions';
 
 class TrackSearchForm extends Component {
+
+  componentDidMount() {
+    this.props.fetchTracks()
+  }
+
   render() {
     return (
       <div className="track-search-form">
-        Search for tracks
+        <h2>Search for tracks</h2>
       </div>
     );
   }
 }
 
-export default TrackSearchForm;
+const mapStateToProps = state => {
+  return {
+    tracks: state.tracks
+  }
+}
+
+export default connect(mapStateToProps, {fetchTracks})(TrackSearchForm);
+/*
+  render() {
+    const { match, playlists } = this.props;
+
+    return (
+      <div className="home-page">
+        <Route exact path={match.url} render={() =>
+            <Link to="/playlists/new">Create a Playlist!</Link>} />
+        <br></br>
+        <Switch>
+          <Route exact path={match.url} render={() =>
+            <PlaylistList playlists={playlists} />} />
+          <Route exact path={match.url + '/new'} component={NewPlaylistForm} />
+          <Route exact path={match.url + '/:playlistId'} component={PlaylistComponent} />
+        </Switch>
+      </div>
+    );
+  }
+}
+*/
