8949a635da2a83a904ec855c526a8c82a65d411b
njpowers7915 <njpowers7915@gmail.com>
2018-10-23 14:36:40 -0400

    Updated Knock initialization file

--- nicks-playlist-app-api/db/schema.rb
+++ nicks-playlist-app-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_130915) do
+ActiveRecord::Schema.define(version: 2018_10_23_183254) do
 
   create_table "playlists", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2018_10_21_130915) do
     t.string "artist"
     t.string "spotify_id"
     t.string "spotify_uri"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
